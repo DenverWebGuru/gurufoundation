a9749b6e09f991478b374fd9a826c77e151c4d4e
Ryan Flynn <social@denverwebguru.com>
2012-04-09 15:36:45 -0600

    shortened folder links and created new index

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width" />
+
+	<title>Welcome to GuruFoundation</title>
+  
+	<!-- Included CSS Files -->
+	<!-- Combine and Compress These CSS Files -->
+	<link rel="stylesheet" href="css/globals.css">
+	<link rel="stylesheet" href="css/typography.css">
+	<link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet" href="css/ui.css">
+	<link rel="stylesheet" href="css/forms.css">
+	<link rel="stylesheet" href="css/orbit.css">
+	<link rel="stylesheet" href="css/reveal.css">
+	<link rel="stylesheet" href="css/mobile.css">
+	<!-- End Combine and Compress These CSS Files -->
+	<link rel="stylesheet" href="css/app.css">
+
+	<!--[if lt IE 9]>
+		<link rel="stylesheet" href="css/ie.css">
+	<![endif]-->
+	
+	<script src="js/modernizr.foundation.js"></script>
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+	<!-- container -->
+	<div class="container">
+		<div class="row">
+			<div class="twelve columns">
+				<h2>Welcome to GuruFoundation</h2>
+				<p>This is version 2.2 released on February 21, 2012</p>
+				<hr />
+			</div>
+		</div>
+	</div>
+	<!-- container -->
+	
+	<!-- Included JS Files -->
+	<script src="js/jquery.min.js"></script>
+	<!-- Combine and Compress These JS Files -->
+	<script src="js/jquery.reveal.js"></script>
+	<script src="js/jquery.orbit-1.4.0.js"></script>
+	<script src="js/jquery.customforms.js"></script>
+	<script src="js/jquery.placeholder.min.js"></script>
+	<script src="js/jquery.tooltips.js"></script>
+	<!-- End Combine and Compress These JS Files -->
+	<script src="js/app.js"></script>
+
+</body>
+</html>
